312efd42bf28f05ae5a1585e9491510dcad97475
Michal Ch <michal.m.chmielewski@gmail.com>
2022-04-28 20:45:37 +0200

    Wrappers example

---
+++ src/main/java/variables/WrappersExample.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class WrappersExample {
+
+    public static void main(String[] args) {
+
+        //Wrappery, czyli klasy reprezentujace typy proste
+        Byte byteNumber = 5;
+        Short shortNumber = 5;
+        Integer intNumber = 5;
+        Long longNumber = 5L;
+        Character sign = 'x';
+        Double doubleNumber = 5.5;
+        Float floatNumber = 5.5f;
+        Boolean logic = true;
+
+    }
+
+}
