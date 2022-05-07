90f202d14b18277f46ab2fc10738ac7349930671
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-07 14:40:46 +0200

    Zadanie if 2

--- src/main/java/conditionalstatement/Exercise2.java
+++ src/main/java/conditionalstatement/Exercise2.java
@@ -13,6 +13,26 @@ public class Exercise2 {
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int c = scanner.nextInt();
+
+        if (a > b && a > c) {
+            if (b > c) {
+                System.out.println(a + " " + b + " " + c);
+            } else {
+                System.out.println(a + " " + c + " " + b);
+            }
+        } else if (b > c) {
+            if (a > c) {
+                System.out.println(b + " " + a + " " + c);
+            } else {
+                System.out.println(b + " " + c + " " + a);
+            }
+        } else {
+            if (a > b) {
+                System.out.println(c + " " + a + " " + b);
+            } else {
+                System.out.println(c + " " + b + " " + a);
+            }
+        }
     }
 
 }
