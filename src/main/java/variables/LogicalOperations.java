098b8457b660d103c6c5fb11c7a3ba22396035cd
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-07 09:58:39 +0200

    Operatory logiczne

---
+++ src/main/java/variables/LogicalOperations.java
@@ -0,0 +1,27 @@
+package variables;
+
+public class LogicalOperations {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        //Operator AND - koniunkcja
+        System.out.println(a && b); //false
+
+        //Operator OR - alternatywa
+        System.out.println(a || b); //trye
+
+        int number = 10;
+
+        System.out.println(number > 5 && number < 20); //true
+
+        System.out.println(number > 100 || number < 15); //true
+
+        //Negacja
+        System.out.println(!a); //false
+    }
+
+
+
+}
