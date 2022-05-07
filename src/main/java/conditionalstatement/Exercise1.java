c6adc85ba0e8d5cff4c093c0b270cfcdb97238f2
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-07 13:39:38 +0200

    Zadanie if

---
+++ src/main/java/conditionalstatement/Exercise1.java
@@ -0,0 +1,20 @@
+package conditionalstatement;
+
+import java.util.Scanner;
+
+/**
+ * Wyświetl największą liczbę.
+ */
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadź 3 cyfry");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        //kod
+    }
+
+}
---
+++ src/main/java/conditionalstatement/Exercise2.java
@@ -0,0 +1,18 @@
+package conditionalstatement;
+
+import java.util.Scanner;
+
+/**
+ * Wyświetl NARAZ liczby od największej do najmniejszej.
+ */
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadź 3 cyfry");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+    }
+
+}
