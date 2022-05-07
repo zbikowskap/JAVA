4b1789c9ebca9502ac1fa24422df9e1b0de8af12
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-07 11:29:03 +0200

    Konwersja

--- src/main/java/variables/ConversionAndCastExample.java
+++ src/main/java/variables/ConversionAndCastExample.java
@@ -1,4 +1,30 @@
 package variables;
 
 public class ConversionAndCastExample {
+
+    public static void main(String[] args) {
+        double numberA = 5.5;
+        int numberB = 3;
+
+        //Następuję automatyczna konwersja dla numberB na double
+        double resultA = numberA + numberB;
+        System.out.println(resultA);
+
+        //Rzutowanie numberA na int, konwersja stratna.
+        int resultB = (int) numberA + numberB;
+        System.out.println(resultB);
+
+        String numberAsString = "15.15";
+        //Konkatenacja
+        System.out.println(numberAsString + 2.5);
+
+        //Konwersja z String na double - Double.parseDouble(numberAsString)
+        double result = Double.parseDouble(numberAsString) + 2.5;
+        System.out.println(result);
+
+        //String to int
+        int n = Integer.parseInt("1234567");
+        System.out.println(n);
+    }
+
 }
