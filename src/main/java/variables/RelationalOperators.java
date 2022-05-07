88aad999fd099c7c55970b7ec757f6fc34dc0145
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-07 09:43:00 +0200

    Operatory relacji

---
+++ src/main/java/variables/RelationalOperators.java
@@ -0,0 +1,25 @@
+package variables;
+
+public class RelationalOperators {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+
+        //Operator == sprawdza czy wartości zmiennych a i b są identyczne
+        boolean result = a == b;
+        System.out.println(result); //false
+
+        //Operator != sprawdza czy wartości są różne
+        System.out.println(a != b); //true
+
+        //Operatory mniejszości/większości
+        System.out.println(a > b); //false
+        System.out.println(a < b); //true
+        System.out.println(a >= 5); //true
+        System.out.println(b <= 6); //false
+
+    }
+
+}
