cdb276bc1bb64f59637eadfd1c42dfde039242ec
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-07 09:22:18 +0200

    Operacje arytmetyczne

---
+++ src/main/java/variables/ArithmeticOperations.java
@@ -0,0 +1,32 @@
+package variables;
+
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+
+        double a = 10;
+        double b = 3;
+
+        double result;
+
+        //10 + 3
+        result = a + b;
+        System.out.println("Suma: " + result);
+
+        //10 - 3
+        result = a - b;
+        System.out.println("Różnica: " + result);
+
+        //10 * 3
+        result = a * b;
+        System.out.println("Iloczyn: " + result);
+
+        //10 / 3
+        result = a / b;
+        System.out.println("Iloraz: " + result);
+
+        result = a % b;
+        System.out.println("Modulo: " + result);
+    }
+
+}
