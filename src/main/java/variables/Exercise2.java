0b6618da90f82a511c7d136f94aef0dfb4006f8e
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-07 10:27:31 +0200

    Km na mile

---
+++ src/main/java/variables/Exercise2.java
@@ -0,0 +1,15 @@
+package variables;
+
+// Kalkulator kilometry - mile
+// Stworz zmienna, ktora przechowa dowolna wartosc w kilometrach.
+// Napisz program, ktory wyswietli wartosc w milach.
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        double km = 100;
+        double result = km * 0.621371192;
+        System.out.println(km + " km to " + result + " mil.");
+    }
+
+
+}
