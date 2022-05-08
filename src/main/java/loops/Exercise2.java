6bf73d4294fbb87b014c1c8e9a513f4e9813d189
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-08 12:52:47 +0200

    Dodanie sout

--- src/main/java/loops/Exercise2.java
+++ src/main/java/loops/Exercise2.java
@@ -6,6 +6,7 @@ public class Exercise2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe: ");
         int bound = scanner.nextInt();
 
         for (int i = 2; i <= bound; i+=2) {
--- src/main/java/loops/Exercise3.java
+++ src/main/java/loops/Exercise3.java
@@ -6,6 +6,7 @@ public class Exercise3 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe: ");
         int side = scanner.nextInt();
 
         //Kwadrat
