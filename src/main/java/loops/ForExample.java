e32805ad641c9d426894a9ec7bd1bb576e13c21d
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-08 09:44:00 +0200

    Przyklad petli for

---
+++ src/main/java/loops/ForExample.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class ForExample {
+
+    public static void main(String[] args) {
+        //3 sekcje - deklaracja zmiennej, warunek, dzialanie na zmiennej
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
+        }
+
+    }
+
+}
