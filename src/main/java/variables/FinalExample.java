ecfa98d4b65a661111e3f42ab3724bea8f61a178
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-08 09:16:45 +0200

    Przyklad stalej

---
+++ src/main/java/variables/FinalExample.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class FinalExample {
+
+    public static void main(String[] args) {
+        //deklaracja stalej
+        final int number = 10;
+
+        //Nie mozna nadpisac stalej zmiennej
+        //number = 15;
+
+        System.out.println(number);
+    }
+
+}
