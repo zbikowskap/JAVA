93d61c46a2dfcf52a54598794852b77d5611d6a7
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-08 14:55:28 +0200

    do while example

---
+++ src/main/java/loops/DoWhileExample.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class DoWhileExample {
+
+    //Petla do while zawsze wykona swoj kod conajmniej raz.
+    public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            System.out.print(i + " ");
+            i++;
+        } while(i < 10);
+
+    }
+
+}
