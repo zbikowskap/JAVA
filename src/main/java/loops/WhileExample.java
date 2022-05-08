9151973ca74098adf3c510d46b32fab785a2b4f8
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-08 12:43:07 +0200

    Przyklad petli while

---
+++ src/main/java/loops/WhileExample.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class WhileExample {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        //while wymaga tylko warunku
+        while (i < 10) {
+            System.out.print(i + " ");
+            //warto pamietac, zeby kiedys warunek sie zmienil na false
+            i++;
+        }
+
+    }
+
+}
