8a6c2bbc3b81dc62dce1bc7e6d93b341ed6ede4a
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-11 17:46:03 +0200

    Zadanie4 cd

--- src/main/java/arrays/Exercise4.java
+++ src/main/java/arrays/Exercise4.java
@@ -5,6 +5,7 @@ public class Exercise4 {
     public static void main(String[] args) {
         int[] array = {5, 10, 5, 10, 5, 10, 5, 10, 5, 10};
 
+        //suma
         int sum = 0;
 
         for (int i = 0; i < array.length; i++) {
@@ -13,8 +14,23 @@ public class Exercise4 {
 
         System.out.println(sum);
 
+        //srednia
         double average = (double) sum / array.length;
         System.out.println(average);
+
+        //srednia z liczb parzystych
+        int sumOfEvenNumbers = 0;
+        int evenNumbersQuantity = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                sumOfEvenNumbers += array[i];
+                evenNumbersQuantity++;
+            }
+        }
+
+        double evenAverage = (double) sumOfEvenNumbers / evenNumbersQuantity;
+        System.out.println(evenAverage);
     }
 
 }
