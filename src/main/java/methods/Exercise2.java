4a847b506616f75539c15a09cab2d60f4a9db9ec
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-12 19:26:17 +0200

    Zadanie 2 metody - tresc

---
+++ src/main/java/methods/Exercise2.java
@@ -0,0 +1,19 @@
+package methods;
+
+public class Exercise2 {
+
+    /**
+     * 1. Zaimplementuj metodę double multiply(double a, double b), która zwróci wynik mnożenia.
+     * 2. Zaimplementuj metodę, która przyjmuję argument String. Metoda powinna zwrócić napis z dużych liter i dodać kropkę na końcu.
+     * 3. Zaimplementuj metodę, która przyjmię jako argument liczbę całkowita n i wyświetli na ekran wszystkie liczby od 1 do n.
+     * 4. Zaimplementuj metodę, która dla podanej jako argument liczby całkowitej zwróci silnie.
+     * 5. Zaimplementuj metodę int sum(int[] array), która obliczy sumę wszystkich elementów w tablicy.
+     *
+     * Działanie metod zaprezentuj w main.
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+}
