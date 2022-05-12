79e8e0c0b291b2786b7821fef205bfc1335b91b0
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-12 20:55:16 +0200

    Zadanie 2 metody

--- src/main/java/methods/Exercise2.java
+++ src/main/java/methods/Exercise2.java
@@ -8,7 +8,7 @@ public class Exercise2 {
      * 3. Zaimplementuj metodę, która przyjmię jako argument liczbę całkowita n i wyświetli na ekran wszystkie liczby od 1 do n.
      * 4. Zaimplementuj metodę, która dla podanej jako argument liczby całkowitej zwróci silnie.
      * 5. Zaimplementuj metodę int sum(int[] array), która obliczy sumę wszystkich elementów w tablicy.
-     *
+     * <p>
      * Działanie metod zaprezentuj w main.
      */
 
@@ -18,6 +18,14 @@ public class Exercise2 {
 
         String decorateResult = decorate("Kolano");
         System.out.println(decorateResult);
+
+        printNumbers(5);
+        printNumbers(3);
+        printNumbers(18);
+
+        int[] tab = {5, 2, 2, 1};
+        int result = sum(tab);
+        System.out.println(result);
     }
 
     public static double multiply(double a, double b) {
@@ -26,6 +34,23 @@ public class Exercise2 {
 
     public static String decorate(String word) {
         return word.toUpperCase() + ".";
+    }
+
+    public static void printNumbers(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static int sum(int[] array) {
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+
+        return sum;
     }
 
 }
