cc4e0165d7ee071a8e38848a15c1c3fc5e351568
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-14 13:52:43 +0200

    Static example

--- src/main/java/oop/basics/StaticExample.java
+++ src/main/java/oop/basics/StaticExample.java
@@ -2,15 +2,18 @@ package oop.basics;
 
 public class StaticExample {
 
+    //statyczne pole
     public static int number = 10;
 
+    //statyczna metoda
     public static void staticMethod() {
         System.out.println("Static method body");
     }
 
     public static void main(String[] args) {
+        //Do pola po nazwie klasy
         System.out.println(StaticExample.number);
-
+        //Dostep do metody bez obiektu, po nazwie klasy
         StaticExample.staticMethod();
     }
 
