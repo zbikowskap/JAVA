f9cca462be1ca41c903acce5aadf29e52b0a3929
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-14 14:26:33 +0200

    Static calculator

---
+++ src/main/java/oop/basics/exercise4/Main.java
@@ -0,0 +1,12 @@
+package oop.basics.exercise4;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double add = StaticCalculator.add(5, 10);
+        System.out.println(add);
+
+    }
+
+}
---
+++ src/main/java/oop/basics/exercise4/StaticCalculator.java
@@ -0,0 +1,9 @@
+package oop.basics.exercise4;
+
+public class StaticCalculator {
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+}
