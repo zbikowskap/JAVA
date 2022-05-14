79b1cc87e5e47ff7098bb2f72bf08b81e9c5b529
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-14 11:54:05 +0200

    Class and object - exercise 1

--- src/main/java/oop/basics/exercise1/Main.java
+++ src/main/java/oop/basics/exercise1/Main.java
@@ -1,9 +1,21 @@
 package oop.basics.exercise1;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-        Employee employee = new Employee("Michal", "Chmielewski");
+        Employee employee1 = new Employee("Michal");
+        Employee employee2 = new Employee("Michal", "Chmielewski");
+        Employee employee3 = new Employee("Michal", "Chmielewski", 28, LocalDate.of(1994, 1, 1), 2500);
+
+        employee1.setFirstname("Cichał");
+        employee1.setLastname("Mhmielewski");
+        System.out.println(employee1.getFirstname());
+        System.out.println(employee1.getLastname());
+
+        employee1.working();
+        employee1.chill();
     }
 
 }
