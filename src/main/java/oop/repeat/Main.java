c3bdd9986fdbe9ce3d1f540944b6f12c97d3d95f
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-15 10:54:52 +0200

    Exercise 1 - repeat

--- src/main/java/oop/repeat/Main.java
+++ src/main/java/oop/repeat/Main.java
@@ -4,8 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Game game = new Game("Terraformacja marsa", 10);
-        System.out.println(game.description());
+        Game terra = new Game("Terraformacja marsa", 10);
+        Game catan = new Game("Catan", 7);
+        Game splendor = new Game("Splendor", 5);
+        
+        Game[] games = {terra, catan, splendor};
+
+        for (int i = 0; i < games.length; i++) {
+            System.out.println(games[i].description());
+        }
 
     }
 
