df51b5ea1ea4bf29b0d56e5810668f7abb38d730
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-18 21:01:10 +0200

    Main do Clinic

---
+++ src/main/java/collections/arraydeque/MainClinic.java
@@ -0,0 +1,18 @@
+package collections.arraydeque;
+
+public class MainClinic {
+
+    public static void main(String[] args) {
+
+        Clinic clinic = new Clinic();
+
+        clinic.registerPatient("Michal");
+        clinic.registerPatient("Romek");
+        clinic.registerPatient("Basia");
+
+        String handledPatient = clinic.handlePatient();
+        System.out.println(handledPatient); //Michal, bo najdluzej stal w kolejce
+
+    }
+
+}
