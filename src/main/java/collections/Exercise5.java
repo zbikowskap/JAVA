c22252b15fd84a8a7c2c6d62204503e1263aa9b9
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-19 20:38:36 +0200

    Zadanie lista

---
+++ src/main/java/collections/Exercise5.java
@@ -0,0 +1,47 @@
+package collections;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Exercise5 {
+
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+
+        words.add("Raz");
+        words.add("Raz");
+        words.add("Dwa");
+        words.add("Trzy");
+        words.add("Raz");
+        words.add("Trzy");
+
+        List<String> result = removeDuplicates(words);
+        System.out.println(result);
+    }
+
+    public static List<String> removeDuplicates(List<String> list) {
+        Set<String> set = new LinkedHashSet<>(list);
+        return new ArrayList<>(set);
+    }
+
+    /*//Za pomoca streamow
+    public static List<String> removeDuplicates(List<String> list) {
+        return list.stream()
+                .distinct()
+                .collect(Collectors.toList());
+    }*/
+
+    //Na piechotę z pętlą
+    /*public static List<String> removeDuplicates(List<String> list) {
+        List<String> result = new ArrayList<>();
+
+        for (String word : list) {
+            if (!result.contains(word)) {
+                result.add(word);
+            }
+        }
+
+        return result;
+    }
+*/
+}
