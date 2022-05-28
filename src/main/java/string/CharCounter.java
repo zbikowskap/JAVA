820b88b3b8a966a197c57a55a193c3c5d3f6b541
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-28 14:43:53 +0200

    CharCounter done

--- src/main/java/string/CharCounter.java
+++ src/main/java/string/CharCounter.java
@@ -3,7 +3,19 @@ package string;
 public class CharCounter {
 
     public static boolean checkXO(String word) {
-        return false;
+        word = word.toLowerCase();
+        int countX = 0;
+        int countO = 0;
+
+        for (char aChar : word.toCharArray()) {
+            if (aChar == 'x') {
+                countX++;
+            } else if (aChar == 'o') {
+                countO++;
+            }
+        }
+
+        return countX == countO;
     }
 
 }
