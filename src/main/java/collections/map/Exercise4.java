2c341c37e185a675eb185359adb44662a3d64671
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-29 15:29:02 +0200

    Zadanie 4 - poczatek

---
+++ src/main/java/collections/map/Exercise4.java
@@ -0,0 +1,20 @@
+package collections.map;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Exercise4 {
+
+    public static void main(String[] args) {
+
+        Map<Student, List<Integer>> studentsWithGrades = new HashMap<>();
+
+        Student student1 = new Student("Michal", "Test", "PL");
+        List<Integer> student1Grades = List.of(5, 3, 3, 2, 5);
+
+        studentsWithGrades.put(student1, student1Grades);
+
+    }
+
+}
