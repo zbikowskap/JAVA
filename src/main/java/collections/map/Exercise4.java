03b53d9660a79e91980043c62cae6d66267ac171
Michal Ch <michal.m.chmielewski@gmail.com>
2022-05-31 17:44:18 +0200

    Zadanie 4

--- src/main/java/collections/map/Exercise4.java
+++ src/main/java/collections/map/Exercise4.java
@@ -13,9 +13,20 @@ public class Exercise4 {
         Student student1 = new Student("Michal", "Test", "PL");
         List<Integer> student1Grades = List.of(5, 3, 3, 2, 5);
 
-        studentsWithGrades.put(student1, student1Grades);
+        Student student2 = new Student("Marcin", "Test", "PL");
+        List<Integer> student2Grades = List.of(2, 3, 3, 2, 2);
 
-        System.out.println(student1);
+        Student student3 = new Student("Basia", "Test", "DE");
+        List<Integer> student3Grades = List.of(5, 4, 4, 4, 5);
+
+        studentsWithGrades.put(student1, student1Grades);
+        studentsWithGrades.put(student2, student2Grades);
+        studentsWithGrades.put(student3, student3Grades);
+
+        for (Map.Entry<Student, List<Integer>> entry : studentsWithGrades.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
     }
 
 }
