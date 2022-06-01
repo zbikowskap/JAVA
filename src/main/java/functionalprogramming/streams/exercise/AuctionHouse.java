e92f1d029b87f505e69c723e348e6fb1e0d09b32
Michal Ch <michal.m.chmielewski@gmail.com>
2022-06-01 19:44:57 +0200

    Room

---
+++ src/main/java/functionalprogramming/streams/exercise/AuctionHouse.java
@@ -0,0 +1,17 @@
+package functionalprogramming.streams.exercise;
+
+import java.util.List;
+
+public class AuctionHouse {
+
+    private List<Room> rooms;
+
+    public AuctionHouse(List<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    public List<Room> findByCity(String city) {
+        return null;
+    }
+
+}
--- src/main/java/functionalprogramming/streams/exercise/Main.java
+++ src/main/java/functionalprogramming/streams/exercise/Main.java
@@ -1,15 +1,23 @@
 package functionalprogramming.streams.exercise;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        List<Room> list = List.of(
+                new Room("Lublin", 10, 1000, 2),
+                new Room("Warszawa", 15, 1400, 3),
+                new Room("Warszawa", 15, 1400, 3),
+                new Room("Gdynia", 11, 1200, 2.5),
+                new Room("Gdynia", 16, 1100, 2.3)
+        );
 
-        Room room1 = new Room("Lublin", 10, 1000, 2);
-        Room room2 = new Room("Warszawa", 15, 1400, 3);
-        Room room3 = new Room("Lublin", 12, 1500, 1);
-        Room room4 = new Room("Gdynia", 11, 1200, 2.5);
-        Room room5 = new Room("Gdynia", 16, 1100, 2.3);
+        AuctionHouse auctionHouse = new AuctionHouse(list);
 
+        List<Room> roomsByCity = auctionHouse.findByCity("Lublin");
+
+        System.out.println(roomsByCity);
     }
 
 }
