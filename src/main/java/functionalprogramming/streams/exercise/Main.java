7a72afc73b45820edbae8f4d22748747107a30ac
Michal Ch <michal.m.chmielewski@gmail.com>
2022-06-01 19:31:07 +0200

    Room

---
+++ src/main/java/functionalprogramming/streams/exercise/Main.java
@@ -0,0 +1,15 @@
+package functionalprogramming.streams.exercise;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Room room1 = new Room("Lublin", 10, 1000, 2);
+        Room room2 = new Room("Warszawa", 15, 1400, 3);
+        Room room3 = new Room("Lublin", 12, 1500, 1);
+        Room room4 = new Room("Gdynia", 11, 1200, 2.5);
+        Room room5 = new Room("Gdynia", 16, 1100, 2.3);
+
+    }
+
+}
---
+++ src/main/java/functionalprogramming/streams/exercise/Room.java
@@ -0,0 +1,32 @@
+package functionalprogramming.streams.exercise;
+
+public class Room {
+
+    private String city;
+    private double m2;
+    private double price;
+    private double distanceFromCenter;
+
+    public Room(String city, double m2, double price, double distanceFromCenter) {
+        this.city = city;
+        this.m2 = m2;
+        this.price = price;
+        this.distanceFromCenter = distanceFromCenter;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public double getM2() {
+        return m2;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getDistanceFromCenter() {
+        return distanceFromCenter;
+    }
+}
